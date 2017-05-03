8349c266014914fc78b18e75caa38965f3f5016b
wangchao3 <wangchao3@ihangmei.com>
2017-05-03 09:43:47 +0800

    大大
    Signed-off-by: wangchao3 <wangchao3@ihangmei.com>

--- TestT/src/com/wang/test/TestStr.java
+++ TestT/src/com/wang/test/TestStr.java
@@ -15,6 +15,7 @@ public class TestStr {
 		System.out.println("耗时: "+(System.currentTimeMillis() - startTimeStamp) + "毫秒");
 		String aa = "T_2.4.6";
 		System.out.println(aa.substring(2));
+        //dfad
 
 	}
 
